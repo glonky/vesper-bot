a6843c28c47fb768ac09997f9b55494ee6418b21
Glonky <glonky.sailor@gmail.com>
2021-10-19 17:30:13 -0700

    feat: use worker on heroku instead of web
    chore: deploy commands

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -1,25 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import fs from 'fs';
+import path from 'path';
 import { REST } from '@discordjs/rest';
-// import { Routes } from 'discord-api-types/v9';
+import { Routes } from 'discord-api-types/v9';
 import Container from 'typedi';
+import glob from 'glob';
 import { Config } from '../config';
 import { Logger } from '../logger';
 
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
 
-// const commands = [];
+const commandFiles = glob.sync(`${commandsPath}/**/*.{js,ts}`, {
+  ignore: ['**/__tests__/**', '**/*.d.ts', '**/*.map.*'],
+});
+
+const commands = [];
 
 for (const file of commandFiles) {
-  // const command = require(`./commands/${file}`);
-  // commands.push(command.data.toJSON());
+  const command = require(file).default;
+  commands.push(command.data.toJSON());
 }
 
 const config = Container.get(Config);
 const rest = new REST({ version: '9' }).setToken(config.discord.token);
 
 const logger = Container.get(Logger);
-// rest
-//   .put(Routes.applicationGuildCommands(config.discord.clientId, config.discord.guildId), { body: commands })
-//   .then(() => logger.info('Successfully registered application commands.'))
-//   .catch(logger.error);
+rest
+  .put(Routes.applicationGuildCommands(config.discord.clientId, config.discord.guildId), { body: commands })
+  .then(() => logger.info('Successfully registered application commands.'))
+  .catch(logger.error);
