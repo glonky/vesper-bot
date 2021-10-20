d52369432ef241e6a2696f6d68f1bfd0930cc99f
Glonky <glonky.sailor@gmail.com>
2021-10-19 17:57:31 -0700

    feat: deploy the commands when building on heroku

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { Message } from 'discord.js';
 
 export default {
-  data: new SlashCommandBuilder().setName('help').setDescription('Replies with Help!'),
+  data: new SlashCommandBuilder().setName('help').setDescription(`Oh no! The table! It's broken!`),
   async execute(message: Message) {
     await message.reply('Help!');
   },
