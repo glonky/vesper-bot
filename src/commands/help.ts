e5d93f3e45865de54df502d126f7ca009aa98cb8
Glonky <glonky.sailor@gmail.com>
2021-10-21 14:36:07 -0700

    feat: test markdown

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,9 +1,34 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
+import {
+  SlashCommandBuilder,
+  bold,
+  italic,
+  strikethrough,
+  underscore,
+  spoiler,
+  quote,
+  blockQuote,
+} from '@discordjs/builders';
 import { Message } from 'discord.js';
+
+const boldString = bold('help me');
+const italicString = italic('the table');
+const strikethroughString = strikethrough("it's broken");
+const underscoreString = underscore('car');
+const spoilerString = spoiler('bar');
+const quoteString = quote('tar');
+const blockquoteString = blockQuote('far');
 
 export default {
   data: new SlashCommandBuilder().setName('help').setDescription(`Oh no! The table! It's broken!`),
   async execute(message: Message) {
-    await message.reply('Help!');
+    await message.reply(`
+    ${boldString}
+    ${italicString}
+    ${strikethroughString}
+    ${underscoreString}
+    ${spoilerString}
+    ${quoteString}
+    ${blockquoteString}
+    `);
   },
 };
