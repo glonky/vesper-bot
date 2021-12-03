5e75ea0bd1c021abf629c6b72f5cfc767ca16070
Glonky <glonky.sailor@gmail.com>
2021-12-02 18:39:56 -0800

    fix: tsc

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,7 +5,7 @@ import { DiscordService } from '../services';
 export default {
   execute() {
     const discordService = Container.get(DiscordService);
-    Container.get(Logger).info(`Ready! Logged in as ${discordService.client.user?.tag}`);
+    Container.get(Logger).info(`Ready! Logged in as ${discordService.client?.user?.tag}`);
   },
   name: 'ready',
   once: true,
