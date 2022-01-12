4d5bd6c83622101973cf545c7ff68d58bdba06ed
Glonky <glonky.sailor@gmail.com>
2022-01-12 11:57:03 -0800

    feat: add jest

---
+++ jest.config.shared.cjs
@@ -0,0 +1,31 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/config.ts',
+    '!<rootDir>/src/**/__tests__/**/*',
+  ],
+  coverageReporters: ['json', 'lcov', 'text-summary', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.global.setup.cjs'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/' ],
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      'esbuild-jest',
+      {
+        sourcemap: true,
+      },
+    ],
+  },
+};
+
+module.exports = config;
