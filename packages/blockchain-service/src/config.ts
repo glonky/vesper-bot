4dac51697f2d9fe313f5bc071afaf215c4764bf1
Glonky <glonky.sailor@gmail.com>
2022-03-15 14:55:33 -0700

    feat: upgrade deps

--- packages/blockchain-service/src/config.ts
+++ packages/blockchain-service/src/config.ts
@@ -2,4 +2,13 @@ import { Service } from 'typedi';
 import { BaseConfig } from '@vesper-discord/config';
 
 @Service()
-export class Config extends BaseConfig {}
+export class Config extends BaseConfig {
+  network = this.getEnvVar<string>('HARDHAT_NETWORK');
+
+  infura = {
+    project: {
+      id: this.getEnvVar<string>('INFURA_PROJECT_ID'),
+      secret: this.getEnvVar<string>('INFURA_PROJECT_SECRET'),
+    },
+  };
+}
