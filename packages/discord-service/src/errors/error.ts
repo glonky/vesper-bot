8db8d2737a68a414c6a1c2ac32341e2b06f1bbb0
Glonky <glonky.sailor@gmail.com>
2022-04-03 15:32:56 -0700

    feat(discord-service): move errors to local folder

---
+++ packages/discord-service/src/errors/error.ts
@@ -0,0 +1,37 @@
+import { ExtendedError, RethrownExtendedErrorProps } from '@vesper-discord/errors';
+
+export type DiscordError = Error & {
+  code: string;
+  requestId: string;
+  retryable: boolean;
+  retryDelay: number;
+  time: string;
+};
+
+export class ExtendedDiscordError extends ExtendedError<DiscordError> {
+  public code: string;
+
+  public requestId: string;
+
+  public retryable: boolean;
+
+  public retryDelay: number;
+
+  public time: string;
+
+  constructor(message: string, props: RethrownExtendedErrorProps<DiscordError>) {
+    super(message, props);
+
+    const error = props.error;
+    this.code = error.code;
+    this.requestId = error.requestId;
+    this.retryable = error.retryable;
+    this.retryDelay = error.retryDelay;
+    this.time = error.time;
+
+    // This is a limitation of typescript and jest
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, ExtendedDiscordError.prototype);
+    this.name = this.constructor.name;
+  }
+}
