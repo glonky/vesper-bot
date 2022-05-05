e93b2d1d37d0ccf8fc82317811b2af03885aeafe
Glonky <glonky.sailor@gmail.com>
2022-05-04 19:13:08 -0700

    fix(redis-serivce): issue with running locally

--- packages/redis-service/src/index.ts
+++ packages/redis-service/src/index.ts
@@ -1,4 +1,4 @@
-export * from './errors';
+export * from './errors/index';
 export * from './service';
 export * from '@type-cacheable/core';
 import CacheManager from '@type-cacheable/core';
--- packages/redis-service/src/service.ts
+++ packages/redis-service/src/service.ts
@@ -1,9 +1,9 @@
 import IoRedis from 'ioredis';
 import { useAdapter } from '@type-cacheable/ioredis-adapter';
 import cacheManager from '@type-cacheable/core';
-import Container, { Service } from 'typedi';
+import { Container, Service } from 'typedi';
 import { Config } from './config';
-import { DefaultStrategy } from './cache-strategies';
+import { DefaultStrategy } from './cache-strategies/index';
 
 @Service()
 export class RedisService {
@@ -15,17 +15,19 @@ export class RedisService {
     }
 
     const config = Container.get(Config);
+    const isLocalhost = config.host.includes('localhost');
     const client = new IoRedis(config.host, {
-      db: config.isTest ? 1 : 0,
-      tls:
-        config.isDevelopment || config.isLocal || config.isTest
-          ? undefined
-          : {
-              rejectUnauthorized: false,
-            },
+      db: config.isTest ? 1 : undefined,
+      keepAlive: isLocalhost ? 1 : 0,
+      tls: isLocalhost
+        ? undefined
+        : {
+            rejectUnauthorized: false,
+          },
     });
     const clientAdapter = useAdapter(client);
     cacheManager.setOptions({
+      debug: config.isDevelopment,
       excludeContext: false,
       strategy: new DefaultStrategy(),
       ttlSeconds: config.ttl,
